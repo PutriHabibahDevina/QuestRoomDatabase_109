6c020ec7ff555b86ca0d7d5f5d35a6955c266b30
PutriHabibahDevina <putrihdm13@gmail.com>
2024-12-14 01:26:14 +0700

    Metode untuk memanggil fungsi getMahasiswa dari MahasiswaDao untuk mengambil data mahasiswa berdasarkan nim.

--- app/src/main/java/com/example/praktikum7/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/praktikum7/repository/RepositoryMhs.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
     fun getAllMahasiswa() : Flow<List<Mahasiswa>>
+    fun getMhs(nim: String): Flow<Mahasiswa>
 }
 
 //Interface itu kayak panduan
