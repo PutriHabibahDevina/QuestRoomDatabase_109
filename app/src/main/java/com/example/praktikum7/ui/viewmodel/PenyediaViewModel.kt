a62e9d194657bf214fd1e6c59c069e1b337455db
PutriHabibahDevina <putrihdm13@gmail.com>
2024-12-16 12:52:34 +0700

    Menambahkan initializer pada PenyediaViewModel untuk inisialisasi ViewModel

--- app/src/main/java/com/example/praktikum7/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/praktikum7/ui/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.praktikum7.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -8,8 +9,32 @@ import com.example.praktikum7.KrsApp
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
+        // MahasiswaViewModel
         initializer {
             MahasiswaViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        // HomeMhsViewModel
+        initializer {
+            HomeMhsViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        // DetailMhsViewModel
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        // UpdateMhsViewModel
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
                 krsApp().containerApp.repositoryMhs
             )
         }
